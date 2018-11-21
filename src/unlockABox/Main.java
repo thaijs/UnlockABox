f3403e7d20be84ea98dd2960084377b2a7b31eb3
bjrohbock <bjrohbock@gmail.com>
2018-11-20 22:35:28 -0700

    ArrayList Test

--- src/unlockABox/Main.java
+++ src/unlockABox/Main.java
@@ -1,10 +1,16 @@
 package unlockABox;
 
+import java.util.ArrayList;
+
 public class Main
 {
 
 	public static void main(String[] args)
 	{
+		ArrayList listd = new ArrayList();
+		listd.add("Bonzai");
+		
+		
 		
 
 	}
