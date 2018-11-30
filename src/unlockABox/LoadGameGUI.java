b88041c084fe38bcbdbaf52c8ca6ad678eb55c03
bjrohbock <bjrohbock@gmail.com>
2018-11-29 22:00:13 -0700

    Updated Load Game menu

--- src/unlockABox/LoadGameGUI.java
+++ src/unlockABox/LoadGameGUI.java
@@ -11,6 +11,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JLabel;
@@ -19,12 +21,17 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import javax.swing.ButtonGroup;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LoadGameGUI extends JFrame
 {
 
 	private JPanel contentPane;
 	private final static ButtonGroup buttonGroup = new ButtonGroup();
+	private String filePath = "Bartholowem.txt";
+	List<String> challengeList = new ArrayList<String>();
+	Scanner sc = new Scanner(filePath);
 
 	/**
 	 * Launch the application.
@@ -50,7 +57,8 @@ public class LoadGameGUI extends JFrame
 	/**
 	 * Create the frame.
 	 */
-	public LoadGameGUI() {
+	public LoadGameGUI()
+	{
 		setTitle("Unlock A Box - Load Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 530);
@@ -58,64 +66,79 @@ public class LoadGameGUI extends JFrame
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel pnlMenu = new JPanel();
 		contentPane.add(pnlMenu, BorderLayout.NORTH);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		pnlMenu.add(menuBar);
-		
+
 		JMenu mnMenu = new JMenu("Menu");
 		menuBar.add(mnMenu);
-		
+
 		JMenuItem mntmNewGame = new JMenuItem("New Game");
 		mnMenu.add(mntmNewGame);
-		
+
 		JMenuItem mntmHelp = new JMenuItem("Help");
 		mnMenu.add(mntmHelp);
-		
+
 		JMenuItem mntmBack = new JMenuItem("Back");
 		mnMenu.add(mntmBack);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mnMenu.add(mntmExit);
-		
+
 		JPanel pnlSavedGames = new JPanel();
 		contentPane.add(pnlSavedGames, BorderLayout.CENTER);
 		pnlSavedGames.setLayout(new GridLayout(3, 0, 0, 0));
-		
+
 		JLabel lblSavedGames = new JLabel("Saved Games");
 		lblSavedGames.setHorizontalAlignment(SwingConstants.CENTER);
 		pnlSavedGames.add(lblSavedGames);
-		
+
 		JPanel pnlSelectGames = new JPanel();
 		pnlSavedGames.add(pnlSelectGames);
 		pnlSelectGames.setLayout(new GridLayout(1, 0, 0, 0));
-		
+
 		/*
-		 * takes in player name?
-		 * need to somehow link name from New Game jFrame
+		 * takes in player name? need to somehow link name from New Game jFrame
 		 */
 		System.out.println("Type Name: ");
 		Scanner sc = new Scanner(System.in);
 		String saveName = sc.nextLine();
-		
+
 //		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
 //		pnlSelectGames.add(rdbtnNewRadioButton);
-		
-		//this may need to be edited
-		JScrollPane scrollPane = new JScrollPane(addGameBtn(pnlSelectGames, saveName));	
+
+		// this may need to be edited
+		JScrollPane scrollPane = new JScrollPane(addGameBtn(pnlSelectGames, saveName));
 		pnlSelectGames.add(scrollPane);
-		
+
 		JButton btnLoad = new JButton("LOAD");
+
 		btnLoad.setFocusable(false);
 		pnlSavedGames.add(btnLoad);
+		btnLoad.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				while (sc.hasNextLine())
+				{
+					challengeList.add(sc.nextLine());
+				}
+				
+				for (String el : challengeList)
+				{
+					System.out.println(el);
+				}
+			}
+		});
 	}
 
 	/**
-	 *Creates a new radio button
-	 *Takes Name of player and applies to new radio button
-	 *saveName should be file path
+	 * Creates a new radio button Takes Name of player and applies to new radio
+	 * button saveName should be file path
+	 * 
 	 * @param panel
 	 * @param saveName
 	 * @return
@@ -124,7 +147,7 @@ public class LoadGameGUI extends JFrame
 	{
 		JRadioButton radioButton = new JRadioButton("Game: " + saveName);
 		buttonGroup.add(radioButton);
-		return panel.add(radioButton);		
+		return panel.add(radioButton);
 	}
-	
+
 }
